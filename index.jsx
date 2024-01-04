1afd65f353aa9cae9505ae085c65ec3e96b401a4
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-01-04 16:56:12 +0200

    ch

--- index.jsx
+++ index.jsx
@@ -108,21 +108,7 @@ const index = () => {
       quibusdam sed reiciendis nobis fugit perspiciatis, tenetur rem laborum
       magni? Corporis minima nobis repellendus incidunt possimus cumque dolore
       earum inventore? Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Aliquid aliquam obcaecati harum eius veniam vitae et voluptate quibusdam
-      nemo? Sit nihil ab tempore quidem molestias quo maxime facere natus porro.
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi obcaecati
-      ab sequi quis beatae iure officia, impedit ut hic incidunt numquam
-      accusantium inventore, eligendi, dolorem quia perspiciatis nobis amet
-      consectetur?
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat iusto
-      quibusdam sed reiciendis nobis fugit perspiciatis, tenetur rem laborum
-      magni? Corporis minima nobis repellendus incidunt possimus cumque dolore
-      earum inventore? Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-      Aliquid aliquam obcaecati harum eius veniam vitae et voluptate quibusdam
-      nemo? Sit nihil ab tempore quidem molestias quo maxime facere natus porro.
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi obcaecati
-      ab sequi quis beatae iure officia, impedit ut hic incidunt numquam
-      accusantium inventore, eligendi, dolorem quia perspiciatis nobis amet
+    
       consectetur?
     </div>
   );
