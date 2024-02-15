38338dce472cfe6d8f6b4e3fe928a6df2cec4c41
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-02-15 09:37:47 +0200

    ch

--- index.tsx
+++ index.tsx
@@ -90,7 +90,7 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, porro arto 
 Lorem ipsum dolor sit, amet conseetur adipisicing elit. Similique hic quia dolores. Ipsum eos ibus omnis quibussumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temporis omnis quibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.
 Lorem ipsum dolor sit, amet consect
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temoribus oquibusdam assumenda quia adipisunde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temorus oquibusdam assumenda quia adipisunde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temporibus omquibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temporibus omnis quibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temporibus omnis quibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
