68ddf2129d07149d948ea9badc12c1fda6fc7074
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-02-22 13:02:46 +0200

    ch

--- index.tsx
+++ index.tsx
@@ -90,7 +90,7 @@ Lorem ipsum dolor sit, amet conseetur adipisicing elit. Similique hic quia dolor
 Lorem ipsum dolor sit, amet consect
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique his. Ipsum eos temorus umnda quiooribus omquibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia doloamribquibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
-Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temporibus omnis quibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Iporibus omnis quibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique hic quia dolores. Ipsum eos temporibus omnis quibusdam assumenda quia adipisci unde exercitationem, consectetur cum dolor debitis itaque, harum non similique.lo
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus incidunt commodi rerum cum. Tempore ab, officiis excepturi blanditiis architecto repudiandae est a ad asperiores perferendis ullam hic quam delectus quia.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus incidunt commodi rerum cum. Tempore ab, officiis excepturi blanditiis architecto repudiandae est a ad asperiores perferendis ullam hic quam delectus quia.
