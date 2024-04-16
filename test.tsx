7ed220d1a75cc7345f85a5cd5c3ff94ca09a5850
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-04-16 08:39:20 +0300

    ch

--- test.tsx
+++ test.tsx
@@ -11,8 +11,7 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam laborovi
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquitae repudiandarovident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitate ali
-Lorem ipsum dolor sit amet consit. Itaque aliquam labrepudianrovm, iste te aliquirepellat  laboriosam dignissimos maxime.t. 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vre ut viandae tempora o reiciendisllat nesciunt veritaime.
+Lorem ipsum dolor sit amet consitipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vre ut viandae tempora o reiciendisllat nesciunt veritaime.
 
 
 git add .
