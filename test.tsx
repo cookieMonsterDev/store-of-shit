e1ebe372966fe7d39f8bb5adf827693f74ca7d65
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-04-18 07:32:46 +0300

    ch

--- test.tsx
+++ test.tsx
@@ -1,7 +1,7 @@
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore utquid iure, repellat nesciunt veritatis sit asectetur adipisicing elit. Itaque aliquam labtae repudiandaovident vero reiciendis a quam 
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore urepellat nesciunt veritatis sit asectetur adipisicing elit. Itaque aliquam labtae repudiandaovident vero reiciendis a quam 
 
 git add .
 git commit -m "ch"
