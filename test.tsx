6f71401622738f410b35ed016c83a88a2e897229
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-04-19 10:29:44 +0300

    ch

--- test.tsx
+++ test.tsx
@@ -1,6 +1,5 @@
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
-Lorem ipsum dolor sit amet consectetur adipisicue aliquam labore urepe
-git add .
+Lorem ipsum dolor sit amet consectetur adipisicue 
 git commit -m "ch"
