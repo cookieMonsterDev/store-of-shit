a0818d517fa1b77cdf37e13c72bb0d149c5c49db
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-04-19 10:30:01 +0300

    ch

--- test.tsx
+++ test.tsx
@@ -1,6 +1,6 @@
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam labore ut vitae repudiandae tempora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis laboriosam dignissimos maxime.
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaqpora provident vero reiciendis a quam laudantium, iste aliquid iure, repellat nesciunt veritatis 
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaqpora provident v laudantium, iste aliquid iure, repellat nesciunt veritatis 
 
 
 
