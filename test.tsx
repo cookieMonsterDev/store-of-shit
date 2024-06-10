2b9a2dab8d9899ef66fd7717b35576d2acfd41a0
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-06-10 09:26:19 +0300

    ch

--- test.tsx
+++ test.tsx
@@ -60,6 +60,6 @@ Lorem ipsum dolor sit amet consectetur adipisicinadiani
 Lorem ipsum dolor sit amet consectetur adipisicinadiani
 Lorem ipsum dolor sit amet consectetur adipisicinadiani
 Lorem ipsum dolor sit amet consectetur adipisicinadiani
-Lorem ipsum dolor sit amet consectetur adipisicinadiani
+Lorem ipsum dolor sit amet consectetur adipi
 
 Lo
