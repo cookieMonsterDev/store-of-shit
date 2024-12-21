578553dd9eb496270e28b6cad565bb244457ae83
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-12-21 20:55:22 +0200

    ch

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const HttpStatus = Object.freeze({
   FORBIDDEN: 403,
   NOT_FOUND: 404,
   METHOD_NOT_ALLOWED: 405,
+  NOT_ACCEPTABLE: 406,
+  REQUEST_TIMEOUT: 408,
   CONFLICT: 409,
   GONE: 410,
   PAYLOAD_TOO_LARGE: 413,
   UNSUPPORTED_MEDIA_TYPE: 415,
+  UNPROCESSABLE_ENTITY: 422,
   TOO_MANY_REQUESTS: 429,
   INTERNAL_SERVER_ERROR: 500,
   NOT_IMPLEMENTED: 501,
@@ -30,10 +33,13 @@ const HttpMessage = Object.freeze({
   FORBIDDEN: "Forbidden",
   NOT_FOUND: "Not Found",
   METHOD_NOT_ALLOWED: "Method Not Allowed",
+  NOT_ACCEPTABLE: "Not Acceptable",
+  REQUEST_TIMEOUT: "Request Timeout",
   CONFLICT: "Conflict",
   GONE: "Gone",
   PAYLOAD_TOO_LARGE: "Payload Too Large",
   UNSUPPORTED_MEDIA_TYPE: "Unsupported Media Type",
+  UNPROCESSABLE_ENTITY: "Unprocessable Entity",
   TOO_MANY_REQUESTS: "Too Many Requests",
   INTERNAL_SERVER_ERROR: "Internal Server Error",
   NOT_IMPLEMENTED: "Not Implemented",
@@ -44,7 +50,7 @@ const HttpMessage = Object.freeze({
 
 class HttpException extends Error {
   constructor(
-    message = HttpMessage.INTERNAL_SERVER_ERROR,
+    message = HttpMessage.INTERNAL_SERVER_ERROR, 
     status = HttpStatus.INTERNAL_SERVER_ERROR,
     error = null
   ) {
@@ -61,7 +67,97 @@ class BadRequestException extends HttpException {
   }
 }
 
-function HttpExceptionsMiddleware(error, _request, response, _next) {
+class UnauthorizedException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.UNAUTHORIZED, HttpStatus.UNAUTHORIZED, error);
+  }
+}
+
+class NotFoundException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.NOT_FOUND, HttpStatus.NOT_FOUND, error);
+  }
+}
+
+class ForbiddenException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.FORBIDDEN, HttpStatus.FORBIDDEN, error);
+  }
+}
+
+class NotAcceptableException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.NOT_ACCEPTABLE, HttpStatus.NOT_ACCEPTABLE, error);
+  }
+}
+
+class RequestTimeoutException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.REQUEST_TIMEOUT, HttpStatus.REQUEST_TIMEOUT, error);
+  }
+}
+
+class ConflictException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.CONFLICT, HttpStatus.CONFLICT, error);
+  }
+}
+
+class GoneException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.GONE, HttpStatus.GONE, error);
+  }
+}
+
+class PayloadTooLargeException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.PAYLOAD_TOO_LARGE, HttpStatus.PAYLOAD_TOO_LARGE, error);
+  }
+}
+
+class UnsupportedMediaTypeException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.UNSUPPORTED_MEDIA_TYPE, HttpStatus.UNSUPPORTED_MEDIA_TYPE, error);
+  }
+}
+
+class UnprocessableEntityException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.UNPROCESSABLE_ENTITY, HttpStatus.UNPROCESSABLE_ENTITY, error);
+  }
+}
+
+class InternalServerErrorException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, error);
+  }
+}
+
+class NotImplementedException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.NOT_IMPLEMENTED, HttpStatus.NOT_IMPLEMENTED, error);
+  }
+}
+
+class MethodNotAllowedException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.METHOD_NOT_ALLOWED, HttpStatus.METHOD_NOT_ALLOWED, error);
+  }
+}
+
+class BadGatewayException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.BAD_GATEWAY, HttpStatus.BAD_GATEWAY, error);
+  }
+}
+
+class GatewayTimeoutException extends HttpException {
+  constructor(error) {
+    super(HttpMessage.GATEWAY_TIMEOUT, HttpStatus.GATEWAY_TIMEOUT, error);
+  }
+}
+
+function HttpExceptionsHandler(error, _request, response, _next) {
   const reason = error.error || "Something went wrong";
   const status = error.status || HttpStatus.INTERNAL_SERVER_ERROR;
   const message = error.message || HttpMessage.INTERNAL_SERVER_ERROR;
